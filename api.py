50a2c91bacd55365dbcacd19f76ef09794455b7f
Kodchakan Prajob <kodchakan.forwork@gmail.com>
2024-03-07 17:05:55 +0700

    json compatible with swift

--- api.py
+++ api.py
@@ -1,16 +1,19 @@
 import json
 from flask import Flask, jsonify
+
 app = Flask(__name__)
 
+
 def get_data(path):
-    with open(f'{path}', 'r', encoding="utf-8") as file:
+    with open(f"{path}", "r") as file:
         data = json.load(file)
     return data
 
-@app.route('/EV/all', methods=['GET'])
+
+@app.route("/EV/all", methods=["GET"])
 def get_json_data():
     json_data = get_data("EV_Charging_Stations/output.json")
     return jsonify(json_data)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- combine_DC_all.py
+++ combine_DC_all.py
@@ -15,10 +15,11 @@ def extract_charging_types(description):
     
     return charging_types
 
-# dc_ev_json = ["Altervim", "EA", "EleX", "etc", "MG", "PEA", "PTT"]
-dc_ev_json = ["PTT"]
+dc_ev_json = ["Altervim", "EA", "EleX", "etc", "MG", "PEA", "PTT"]
+# dc_ev_json = ["PTT"]
 output = {"results": []}
 output_file_path = Path('EV_Charging_Stations/output.json')
+
 # Add up json from folder DC_EV_Charging_Station_Thailand
 for ev_json in dc_ev_json:
     file_path = Path(f'DC_EV_Charging_Station_Thailand/{ev_json}.geojson')
@@ -26,7 +27,6 @@ for ev_json in dc_ev_json:
         data = json.load(file)
         for chunk in data['features']:
             # print(chunk['properties']['description'])
-
             output["results"].append(chunk['properties'])
 
         
@@ -35,7 +35,7 @@ with open(output_file_path, 'w', encoding="utf-8") as output_file:
     json.dump(output, output_file, indent=4, ensure_ascii=False)
 print("Output file created:", output_file_path)
 
-
+# Test Cases
 print(extract_charging_types("2x CCS2: 150 kW (1000V 200A), Type2: 11 kW"))
 print(extract_charging_types("CCS2: 2x 50 kW\nType2: CLOSED\nParking"))
 print(extract_charging_types("Type2: 22 kW\nCCS2: 2x 75 kW\nParking"))
